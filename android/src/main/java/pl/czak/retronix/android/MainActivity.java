baf2c87b433db469238bcad62b443c9ad817bdbc
Łukasz Adamczak <lukasz@czak.pl>
2016-04-14 12:57:24 +0200

    D-pad controls on Android

--- android/src/main/java/pl/czak/retronix/android/MainActivity.java
+++ android/src/main/java/pl/czak/retronix/android/MainActivity.java
@@ -2,12 +2,26 @@ package pl.czak.retronix.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.KeyEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import pl.czak.retronix.Direction;
 import pl.czak.retronix.Game;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
+    private static Map<Integer, Direction> DIRECTION_MAP = new HashMap<>();
+
+    static {
+        DIRECTION_MAP.put(KeyEvent.KEYCODE_DPAD_UP, Direction.NORTH);
+        DIRECTION_MAP.put(KeyEvent.KEYCODE_DPAD_DOWN, Direction.SOUTH);
+        DIRECTION_MAP.put(KeyEvent.KEYCODE_DPAD_RIGHT, Direction.EAST);
+        DIRECTION_MAP.put(KeyEvent.KEYCODE_DPAD_LEFT, Direction.WEST);
+    }
+
+    private Game game;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +29,18 @@ public class MainActivity extends Activity
         Screen screen = new Screen(this);
         setContentView(screen);
 
-        Game game = new Game(screen);
+        game = new Game(screen);
         game.start();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Direction dir = DIRECTION_MAP.get(keyCode);
+        if (dir != null) {
+            game.setPlayerDirection(dir);
+            return true;
+        }
+
+        return false;
     }
 }
