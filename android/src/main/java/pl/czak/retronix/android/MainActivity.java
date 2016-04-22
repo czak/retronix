dd85a6344049cd11219574369e6086f67a15bbd6
Łukasz Adamczak <lukasz@czak.pl>
2016-04-22 21:11:21 +0200

    Fix android build; initial CanvasRenderer for android

--- android/src/main/java/pl/czak/retronix/android/MainActivity.java
+++ android/src/main/java/pl/czak/retronix/android/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import pl.czak.retronix.Game;
+import pl.czak.retronix.engine.State;
+import pl.czak.retronix.engine.Backend;
+import pl.czak.retronix.engine.Event;
 import pl.czak.retronix.states.WelcomeState;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Backend {
     private Game game;
     private Screen screen;
 
@@ -17,7 +20,7 @@ public class MainActivity extends Activity {
         screen = new Screen(this);
         setContentView(screen);
 
-        game = new Game();
+        game = new Game(this);
         game.pushState(new WelcomeState(game));
 
         // Main loop in a separate thread
@@ -27,9 +30,9 @@ public class MainActivity extends Activity {
                 while (true) {
                     long start = System.currentTimeMillis();
 
-                    game.handleEvents();
+                    game.handleEvent();
                     game.update();
-                    screen.draw(game.getCurrentState());
+                    game.draw();
 
                     long duration = System.currentTimeMillis() - start;
 
@@ -43,6 +46,35 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return game.setKeyEvent(event);
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+                game.addEvent(Event.KEY_UP);
+                return true;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                game.addEvent(Event.KEY_DOWN);
+                return true;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                game.addEvent(Event.KEY_LEFT);
+                return true;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                game.addEvent(Event.KEY_RIGHT);
+                return true;
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_ENTER:
+                game.addEvent(Event.KEY_SELECT);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void playSound(Game.Sound sound) {
+        System.out.println("Playing sound: " + sound.toString());
+    }
+
+    @Override
+    public void draw(State state) {
+        screen.draw(state);
     }
 }
--- android/src/main/java/pl/czak/retronix/android/Screen.java
+++ android/src/main/java/pl/czak/retronix/android/Screen.java
@@ -2,33 +2,66 @@ package pl.czak.retronix.android;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import pl.czak.retronix.State;
+import pl.czak.retronix.engine.Renderer;
+import pl.czak.retronix.engine.State;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by czak on 14/04/16.
  */
 public class Screen extends SurfaceView implements SurfaceHolder.Callback {
+    // Prebuilt Paint objects to use for rendering
+    private static final Map<Renderer.Color, Paint> PAINT_MAP = new HashMap<>();
+    static {
+        Paint p = new Paint();
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(Color.WHITE);
+        PAINT_MAP.put(Renderer.Color.WHITE, p);
+
+        p = new Paint();
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(0xff00a8a8);
+        PAINT_MAP.put(Renderer.Color.CYAN, p);
+
+        p = new Paint();
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(0xffa800a8);
+        PAINT_MAP.put(Renderer.Color.MAGENTA, p);
+    }
+
     private SurfaceHolder holder;
     private boolean ready;
+
+    private Canvas canvas;
+    private CanvasRenderer renderer;
 
     public Screen(Context context) {
         super(context);
         holder = getHolder();
         holder.addCallback(this);
         holder.setFixedSize(1280, 720);
+
+        this.renderer = new CanvasRenderer();
     }
 
     public void draw(State state) {
         if (!ready) return;
 
-        Canvas canvas = holder.lockCanvas();
-        state.render(canvas);
+        canvas = holder.lockCanvas();
+        canvas.scale(4, 4);
+        canvas.drawColor(Color.BLACK);
+        state.render(renderer);
         holder.unlockCanvasAndPost(canvas);
     }
 
+    // region ----- SurfaceHolder.Callback methods -----
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         ready = true;
@@ -42,5 +75,29 @@ public class Screen extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         ready = false;
+    }
+    // endregion
+
+    class CanvasRenderer implements Renderer {
+        @Override
+        public void drawSprite(int x, int y, int spriteId) {
+            canvas.drawRect(x, y, x+4, y+4, PAINT_MAP.get(Color.WHITE));
+        }
+
+        @Override
+        public void drawString(int x, int y, String text) {
+            for (byte b : text.getBytes(StandardCharsets.ISO_8859_1)) {
+                int index = b & 0xff;
+                int sx = (index % 16) * 8;
+                int sy = (index / 16) * 8;
+                canvas.drawRect(x, y, x+8, y+8, PAINT_MAP.get(Color.WHITE));
+                x+=8;
+            }
+        }
+
+        @Override
+        public void fillRect(int x, int y, int width, int height, Color color) {
+            canvas.drawRect(x, y, x+width, y+height, PAINT_MAP.get(color));
+        }
     }
 }
